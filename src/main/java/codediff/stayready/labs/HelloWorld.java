716ff80d22ea8f01c1823164ed4c2c0d7e671645
Nigel Jennings <nljennings19@students.desu.edu@Nigels-MacBook-Pro-3.local>
2021-04-20 10:31:33 -0400

    First Commit

---
+++ src/main/java/codediff/stayready/labs/HelloWorld.java
@@ -0,0 +1,13 @@
+package codediff.stayready.labs;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Name: Nigel Jennings");
+        System.out.println("Hometown: Bowie, MD");
+        System.out.println("Major: Computer Science");
+        System.out.println("Favorite Song: Flava in ya ear remix by Craig Mack");
+        System.out.println("Favorite Superhero(s): Ironman and thor");
+        System.out.println("Favorite Caffeine source: Definitely iced caramel coffee");
+    }
+}
